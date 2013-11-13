e4391ac240be818eb12ac5e221fa74df30b159bd
Nicholas Clawson <nickclaw@gmail.com>
2013-11-12 18:25:53 -0800

    Added some useful shortcuts/prototypes

--- popup.js
+++ popup.js
@@ -1,23 +1,26 @@
-function handleClick(event) {
-	var windows = chrome.windows.getAll(null, arrangeWindows);
+/************ SHORT CUTS & PROTOTYPES ***************/
+function getWindows(callback) {
+	chrome.windows.getAll(null, callback);
 }
 
-function arrangeWindows(windows) {
-	var width = screen.width;
-	var height = screen.availHeight;
-	var numWindows = windows.length;
-	a = windows;
-	for(var i = 0; i < numWindows; i++) {
-		chrome.windows.update(windows[i].id, {
-			'left': width/2 * i,
-			'top': 0,
-			'width': width/2,
-			'height': height,
-			'drawAttention': true,
-			'state': 'normal'
-		})
+function updateWindow(window, data) {
+	chrome.windows.update(window.id, data);
+}
+
+Array.prototype.each = function(callback, context) {
+	for (var i = 0; i < this.length; i++) {
+		callback.call(context, i, this[i]);
 	}
 }
+Object.prototype.each = function(callback, context) {
+	for (key in this) {
+		if (this.hasOwnProperty(key)) {
+			callback.call(context, key, this[key]);
+		}
+	}
+}
+
+
 
 function calc(n, margin) {
 	return 'calc(' + (n * 100) + '% - ' + margin + 'px )';
@@ -27,15 +30,20 @@ function createIcon(data) {
 	var element = document.createElement('div');
 	element.classList.add('window');
 
-	for (var i = 0; i < data.length; i++) {
+	data.each(function(key, value) {
 		var sub = document.createElement('div');
 		sub.classList.add('sub');
-		sub.style.left = calc(data[i].x, 0);
-		sub.style.top = calc(data[i].y, 0);
-		sub.style.width = calc(data[i].w, 10);
-		sub.style.height = calc(data[i].h, 10);
+		sub.style.left = calc(value.x, 0);
+		sub.style.top = calc(value.y, 0);
+		sub.style.width = calc(value.w, 10);
+		sub.style.height = calc(value.h, 10);
 		element.appendChild(sub);
-	}
+	});
+
+	element.addEventListener('click', function(event) {
+		setWindows(data);
+		window.close();
+	});
 
 	return element;
 }
@@ -44,41 +52,26 @@ function setWindows(data) {
 	var width = screen.width;
 	var height = screen.availHeight;
 
-	chrome.windows.getAll(null, function(windows) {
-		console.log(windows, data);
-		for (var i = 0; i < windows.length; i++) {
-			chrome.windows.update(windows[i].id, {
+	getWindows(function(windows) {
+		windows.each(function(i, value) {
+			updateWindow(value, {
 				'left': Math.floor(width * data[i].x),
 				'top': Math.floor(height * data[i].y),
 				'width': Math.floor(width * data[i].w),
 				'height': Math.floor(height * data[i].h)
 			});
-		}
+		});
 	});
 }
 
-function setOptions() {
-	var windows = chrome.windows.getAll(null, function(windows) {
-		var choices = options[windows.length];
-		
-		for (var i = 0; i < choices.length; i++) {
-			var element = createIcon(choices[i]);
-			element.addEventListener('click', (function(settings) {
-				return function(event) {
-					setWindows(settings);
-					window.close();
-				}
-			})(choices[i]));
-			document.body.appendChild(element);
-		}
+function fillBody() {
+	getWindows(function(windows) {
+		options[windows.length].each(function(key, value) {
+			document.body.appendChild(createIcon(value));
+		});
 	});
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-	setOptions();
-
-	var buttons = document.querySelectorAll('.window');
-	for(var i = 0; i < buttons.length; i++) {
-		buttons[i].addEventListener('click', handleClick);
-	}
+	fillBody();
 });
